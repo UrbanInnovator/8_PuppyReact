b61ce3d9b35e9d9bca85761d679def4f8b815d8b
supasage <suburbaninventor@gmail.com>
2023-03-14 23:01:59 -0500

    incomplete but comprehended

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
-  const [puppyBowl, setPuppyBowl] = useState([])
+  const [puppyBowl, setPuppies] = useState([])
 
   useEffect(() => {
     const getPuppies = async() => {
@@ -10,8 +10,8 @@ const App = () => {
         const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2211-FTB-ET-WEB-AM/players');
         const data = await response.json();
   
-        setPuppyBowl(data.data.players)
-        console.log(puppyBowl);
+        setPuppies(data.data.players);
+        console.log(data.data.players[0]);
       } catch (error) {
         throw error;
       }
@@ -21,11 +21,20 @@ const App = () => {
 
   return (
     <>
-    {
-      puppyBowl.map((singlePup, index) => {
-        return <li key={index}>{singlePup.name}</li>
-      })
-    }
+      <div id="PupPage">
+        {
+          puppyBowl.map((singlePup, index) => {
+            return (
+              <div class="OnePup" key={index}>
+                <h3 class="PupInfo">ID#: {singlePup.id} | {singlePup.name}</h3>
+                <img class="PupPic" src={singlePup.imageUrl}/>
+                <h3 class="PupInfo">Pup Stats</h3>
+                <h4 class="PupStats">Breed: {singlePup.breed} | Current Status: {singlePup.status}</h4>
+              </div>
+            )
+          })
+        }
+      </div>
     </>
   )
 
